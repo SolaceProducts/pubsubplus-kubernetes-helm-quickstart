f8b2e6b3b7f5df772395c791138a16c450415d24
KenBarr <ken.barr@solace>
2018-03-15 16:55:38 -0400

    Fix reference to secrets.yaml and GCE disk provisiner

--- scripts/start_vmr.sh
+++ scripts/start_vmr.sh
@@ -102,8 +102,8 @@ IFS=':' read -ra container_array <<< "$solace_image"
 sed ${sed_options} "s:SOLOS_IMAGE_REPO:${container_array[0]}:g" values.yaml
 sed ${sed_options} "s:SOLOS_IMAGE_TAG:${container_array[1]}:g"  values.yaml
 sed ${sed_options} "s/SOLOS_CLOUD_PROVIDER/${cloud_provider}/g"  values.yaml
-sed ${sed_options} "s/SOLOS_ADMIN_PASSWORD/${solace_password}/g" templates/pre-install-secret.yaml
-rm templates/pre-install-secret.yaml.bak
+sed ${sed_options} "s/SOLOS_ADMIN_PASSWORD/${solace_password}/g" templates/secret.yaml
+rm templates/secret.yaml.bak
 
 echo "`date` INFO: DEPLOY VMR TO CLUSTER"
 echo "#############################################################"
