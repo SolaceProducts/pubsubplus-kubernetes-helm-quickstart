0d26208d508620d0f3c223f6d6ebb77ab3046f13
bczoma <balazs.czoma@solace.com>
2019-12-17 10:56:37 -0500

    Adding test coverage for latest published charts

---
+++ docs/helm-charts/test-chart-variants-from-gh-pages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Test latest published Solace pubsubplus Helm chart variants from gh-pages, fails out on error
+#   tests pubsubplus, pubsubplus-dev, pubsubplus-ha with Helm v2 and Helm v3
+# Params:
+#   $1: the published location of the Solace URL
+# Assumes being run on a Kubernetes environment with enough resources for HA dev deployment
+#   - kubectl configured
