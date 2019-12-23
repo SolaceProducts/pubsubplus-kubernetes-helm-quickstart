d843a288ddb43df6f5eca46289c0e6277c82e16b
bczoma <balazs.czoma@solace.com>
2019-12-23 13:53:12 -0500

    Fixed use of colon in Chart.yaml failing packaging

--- docs/helm-charts/create-chart-variants.sh
+++ docs/helm-charts/create-chart-variants.sh
@@ -6,7 +6,7 @@
 
 # Customize pubsubplus-ha
 cp -r pubsubplus/ pubsubplus-ha/
-sed -i 's/description:.*$/description: Deploy an HA redundancy group of Solace PubSub+ Event Broker: Software onto a Kubernetes Cluster/g' pubsubplus-ha/Chart.yaml
+sed -i 's/description:.*$/description: Deploy an HA redundancy group of Solace PubSub+ Event Broker Software onto a Kubernetes Cluster/g' pubsubplus-ha/Chart.yaml
 sed -i '/name:/ s/pubsubplus/pubsubplus-ha/g' pubsubplus-ha/Chart.yaml
 # no need to update solace.size and storage.size
 sed -i 's/redundancy:.*$/redundancy: true/g' pubsubplus-ha/values.yaml
@@ -17,7 +17,7 @@ helm package pubsubplus-ha
 
 # Customize pubsubplus-dev
 cp -r pubsubplus/ pubsubplus-dev/
-sed -i 's/description:.*$/description: Deploy a minimum footprint single-node non-HA Solace PubSub+ Event Broker: Software onto a Kubernetes Cluster for development purposes/g' pubsubplus-dev/Chart.yaml
+sed -i 's/description:.*$/description: Deploy a minimum footprint single-node non-HA Solace PubSub+ Event Broker Software onto a Kubernetes Cluster for development purposes/g' pubsubplus-dev/Chart.yaml
 sed -i '/name:/ s/pubsubplus/pubsubplus-dev/g' pubsubplus-dev/Chart.yaml
 sed -i 's/size: prod.*$/size: dev/g' pubsubplus-dev/values.yaml
 sed -i 's/size: .*Gi/size: 10Gi/g' pubsubplus-dev/values.yaml
@@ -30,7 +30,7 @@ helm package pubsubplus-dev
 
 # Customize pubsubplus
 # no need to update Chart.yaml name
-sed -i 's/description:.*$/description: Deploy a single-node non-HA Solace PubSub+ Event Broker: Software onto a Kubernetes Cluster/g' pubsubplus/Chart.yaml
+sed -i 's/description:.*$/description: Deploy a single-node non-HA Solace PubSub+ Event Broker Software onto a Kubernetes Cluster/g' pubsubplus/Chart.yaml
 # no need to update values.yaml solace.redundancy, solace.size and storage.size
 sed -i 's/This chart bootstraps a single-node or HA deployment$/This chart bootstraps a single-node deployment/g' pubsubplus/README.md
 helm package pubsubplus
