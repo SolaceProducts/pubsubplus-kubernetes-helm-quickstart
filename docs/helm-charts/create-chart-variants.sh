4bc91ab9be738ed2ab2e17675d852bdd3856e8ae
bczoma <balazs.czoma@solace.com>
2022-01-19 11:29:40 -0500

    Fixes to create chart variants script

--- docs/helm-charts/create-chart-variants.sh
+++ docs/helm-charts/create-chart-variants.sh
@@ -7,7 +7,7 @@
 # Customize pubsubplus-ha
 cp -r pubsubplus/ pubsubplus-ha/
 sed -i 's/description:.*$/description: Deploy an HA redundancy group of Solace PubSub+ Event Broker Software onto a Kubernetes Cluster/g' pubsubplus-ha/Chart.yaml
-sed -i 's@charts.openshift.io/name@charts.openshift.io/name: PubSub+ Event Broker, HA@g' pubsubplus-ha/Chart.yaml
+sed -i '/charts.openshift.io\/name:/ s@charts.openshift.io/name.*@charts.openshift.io/name: PubSub+ Event Broker, HA@g' pubsubplus-ha/Chart.yaml
 sed -i '/name:/ s/pubsubplus/pubsubplus-ha/g' pubsubplus-ha/Chart.yaml
 # no need to update solace.size and storage.size
 sed -i 's/redundancy:.*$/redundancy: true/g' pubsubplus-ha/values.yaml
@@ -19,7 +19,7 @@ helm package pubsubplus-ha
 # Customize pubsubplus-dev
 cp -r pubsubplus/ pubsubplus-dev/
 sed -i 's/description:.*$/description: Deploy a minimum footprint single-node non-HA Solace PubSub+ Event Broker Software onto a Kubernetes Cluster for development purposes/g' pubsubplus-dev/Chart.yaml
-sed -i 's@charts.openshift.io/name@charts.openshift.io/name: PubSub+ Event Broker for Developers@g' pubsubplus-dev/Chart.yaml
+sed -i '/charts.openshift.io\/name:/ s@charts.openshift.io/name.*@charts.openshift.io/name: PubSub+ Event Broker for Developers@g' pubsubplus-dev/Chart.yaml
 sed -i '/name:/ s/pubsubplus/pubsubplus-dev/g' pubsubplus-dev/Chart.yaml
 sed -i 's/size: prod.*$/size: dev/g' pubsubplus-dev/values.yaml
 sed -i 's/size: .*Gi/size: 10Gi/g' pubsubplus-dev/values.yaml
@@ -32,7 +32,7 @@ helm package pubsubplus-dev
 
 # Customize pubsubplus
 sed -i 's/description:.*$/description: Deploy a single-node non-HA Solace PubSub+ Event Broker Software onto a Kubernetes Cluster/g' pubsubplus/Chart.yaml
-sed -i 's@charts.openshift.io/name@charts.openshift.io/name: PubSub+ Event Broker, non-HA@g' pubsubplus/Chart.yaml
+sed -i '/charts.openshift.io\/name:/ s@charts.openshift.io/name.*@charts.openshift.io/name: PubSub+ Event Broker, non-HA@g' pubsubplus/Chart.yaml
 # no need to update Chart.yaml name
 # no need to update values.yaml solace.redundancy, solace.size and storage.size
 sed -i 's/This chart bootstraps a single-node or HA deployment/This chart bootstraps a single-node deployment/g' pubsubplus/README.md
