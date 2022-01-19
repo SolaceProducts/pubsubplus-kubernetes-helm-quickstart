7c1ce5532f0e684fd83d1ffd542e8ac36da5f1a5
bczoma <balazs.czoma@solace.com>
2022-01-19 10:46:04 -0500

    Customize chart names for OpenShift

--- docs/helm-charts/create-chart-variants.sh
+++ docs/helm-charts/create-chart-variants.sh
@@ -7,6 +7,7 @@
 # Customize pubsubplus-ha
 cp -r pubsubplus/ pubsubplus-ha/
 sed -i 's/description:.*$/description: Deploy an HA redundancy group of Solace PubSub+ Event Broker Software onto a Kubernetes Cluster/g' pubsubplus-ha/Chart.yaml
+sed -i 's@charts.openshift.io/name@charts.openshift.io/name: PubSub+ Event Broker, HA@g' pubsubplus-ha/Chart.yaml
 sed -i '/name:/ s/pubsubplus/pubsubplus-ha/g' pubsubplus-ha/Chart.yaml
 # no need to update solace.size and storage.size
 sed -i 's/redundancy:.*$/redundancy: true/g' pubsubplus-ha/values.yaml
@@ -17,20 +18,22 @@ helm package pubsubplus-ha
 
 # Customize pubsubplus-dev
 cp -r pubsubplus/ pubsubplus-dev/
-sed -i 's/description:.*$/description: Deploy a single-node non-HA Solace PubSub+ Event Broker Software onto a Kubernetes Cluster for development purposes/g' pubsubplus-dev/Chart.yaml
+sed -i 's/description:.*$/description: Deploy a minimum footprint single-node non-HA Solace PubSub+ Event Broker Software onto a Kubernetes Cluster for development purposes/g' pubsubplus-dev/Chart.yaml
+sed -i 's@charts.openshift.io/name@charts.openshift.io/name: PubSub+ Event Broker for Developers@g' pubsubplus-dev/Chart.yaml
 sed -i '/name:/ s/pubsubplus/pubsubplus-dev/g' pubsubplus-dev/Chart.yaml
 sed -i 's/size: prod.*$/size: dev/g' pubsubplus-dev/values.yaml
 sed -i 's/size: .*Gi/size: 10Gi/g' pubsubplus-dev/values.yaml
 sed -i 's/# Solace PubSub+ Message Broker Helm Chart/# Solace PubSub+ Message Broker Helm Chart for Developers/g' pubsubplus-dev/README.md
-sed -i 's/This chart bootstraps a single-node or HA deployment/This chart bootstraps single-node deployment for Developers/g' pubsubplus-dev/README.md
+sed -i 's/This chart bootstraps a single-node or HA deployment/This chart bootstraps minimum single-node deployment for Developers/g' pubsubplus-dev/README.md
 sed -i 's@solacecharts/pubsubplus@solacecharts/pubsubplus-dev@g' pubsubplus-dev/README.md
 sed -i '/`solace.size`/ s/| `prod.*` |/| `dev` |/g' pubsubplus-dev/README.md
 sed -i '/`storage.size`/ s/| `..Gi` |/| `10Gi` |/g' pubsubplus-dev/README.md
 helm package pubsubplus-dev
 
 # Customize pubsubplus
-# no need to update Chart.yaml name
 sed -i 's/description:.*$/description: Deploy a single-node non-HA Solace PubSub+ Event Broker Software onto a Kubernetes Cluster/g' pubsubplus/Chart.yaml
+sed -i 's@charts.openshift.io/name@charts.openshift.io/name: PubSub+ Event Broker, non-HA@g' pubsubplus/Chart.yaml
+# no need to update Chart.yaml name
 # no need to update values.yaml solace.redundancy, solace.size and storage.size
 sed -i 's/This chart bootstraps a single-node or HA deployment/This chart bootstraps a single-node deployment/g' pubsubplus/README.md
 helm package pubsubplus
